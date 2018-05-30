ce2d1ef1dd76923e65eca792875d9818f9c416a0
Raphael Poli <raphaelpoli@wanadoo.fr>
2018-05-30 11:30:55 +0200

    this is my submission with comment

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,9 @@
 ## functions do
 
 ## Write a short comment describing this function
+#This function returns a list of four functions that allow to store 
+#a chosen matrix in the parent environement, to read it and to transfer the inverse 
+#of the matrix to the i variable
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -21,6 +24,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# this function verfies if i is already calculated or null, 
+#if it is null, (the looked up matrix has been reassigned or just assigned)
+#it computes the inverse and transfers it to the root i via the above function
+#else it retreives it in parent environment variable i and stops
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
